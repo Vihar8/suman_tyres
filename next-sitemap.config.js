e6f253f37958f19a8d53be5415ae9e958c77eadb
Vihar8 <168709645+Vihar8@users.noreply.github.com>
2024-07-17 10:04:19 +0530

    edited seo

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://sumantyres.vercel.app', // Your website URL
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
+  
