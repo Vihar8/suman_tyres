da4aa66469a7ccb5ec21b492393753d87f47b10f
Vihar8 <168709645+Vihar8@users.noreply.github.com>
2024-07-17 09:19:42 +0530

    desc

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Suman Tyres",
-  description: "We are wholesaler Sell We Help People To Buy!",
+  description: "We Help People To Buy!",
   keywords: "tyres, bike tyre, activa tyre, car tyre, wiper blade shop,car battery charging , engine oil, car battery,  wholesale tyres, tyre shop Gandhinagar, tyre wholesaler, Gandhinagar tyres, best tyre shop in gandhinagar, tyre shop near me",
 };
 
